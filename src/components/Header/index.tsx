c3ab005bc144252ab26f20939d26d1ec39b8952f
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2023-02-05 20:14:28 -0300

    Finished front interface

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SigInButton } from './SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SigInButton />
+      </div>
+    </header>
+  )
+}
